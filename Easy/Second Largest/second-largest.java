1735cb14e46cb8378838519e2f02a10d4c824085
AAYUSH UPADHYAY <144820397+uaayush01@users.noreply.github.com>
2024-06-10 00:23:42 -0400

    Added Solution - GfG to GitHub

---
+++ Easy/Second Largest/second-largest.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.stream.Collectors; 
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().print2largest(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int print2largest(int arr[], int n) {
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > first) {
+                second = first;
+                first = arr[i];
+            } else if (arr[i] > second && arr[i] != first) {
+                second = arr[i];
+            }
+        }
+        
+        if (second == Integer.MIN_VALUE) {
+            return -1;
+        } else {
+            return second;
+        }
+    }
+}
+
